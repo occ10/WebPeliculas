d2226b0994daa4478395fd695cfc2b3ecb0fa051
CHAMIT\walid <occ10@alu.ua.es>
2017-01-26 15:09:30 +0100

    crear casos de uso

--- application/views/public/includes/menu.php
+++ application/views/public/includes/menu.php
@@ -18,6 +18,14 @@
                         <li class="<?php if($this->uri->segment(1)==""){echo "active";}?>"><a href="<?php echo site_url('/')?>">Inicio</a></li>
                         <li class="<?php if($this->uri->segment(1)=="listadopeliculas"){echo "active";}?>"><a href="<?php echo site_url('listadopeliculas')?>">Listado películas</a></li>
                         <li class="<?php if($this->uri->segment(1)=="contacto"){echo "active";}?>"><a href="<?php echo site_url('contacto')?>">Contacto</a></li>
+                        <?php
+
+                        if ($this->session->userdata('user') && $this->session->userdata('user')->tipo==1) {
+                        ?>
+                            <li class="<?php if($this->uri->segment(1)=="private"){echo "active";}?>"><a href="<?php echo site_url('private')?>">Administrar</a></li>
+                        <?php
+                        }
+                        ?>
                     </ul>
                 </nav>
             </div>
